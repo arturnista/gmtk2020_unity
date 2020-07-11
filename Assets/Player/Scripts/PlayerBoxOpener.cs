83f2e3bb767a8f3e763588baa60d2e0ca6dcb41c
Carlos Alexandre Zgierski <carloszgierski@gmail.com>
2020-07-11 20:27:56 -0300

    Fixed infinite item spawn.

--- Assets/Player/Scripts/PlayerBoxOpener.cs
+++ Assets/Player/Scripts/PlayerBoxOpener.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class PlayerBoxOpener : MonoBehaviour
 {
-    private bool canOpen;
     [SerializeField]
     private List <ItemData> toolsPrefabs;
+    private Collider2D savedCollider;
+
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F) && canOpen)
+        if(Input.GetKeyDown(KeyCode.F) && savedCollider != null)
         {
             Debug.Log("aperto");
             toolsPrefabs[Random.Range(0, toolsPrefabs.Count)].Create(transform.position);
+            Destroy(savedCollider.gameObject);
         }
     }
     
     void OnTriggerEnter2D(Collider2D collider2D)
     {
-        if(collider2D.gameObject.tag == "ItemBox")
+        if(collider2D.tag == "ItemBox")
         {
+            savedCollider = collider2D;
             Debug.Log("colidiu");
-            canOpen = true;
         }
     }
 }
