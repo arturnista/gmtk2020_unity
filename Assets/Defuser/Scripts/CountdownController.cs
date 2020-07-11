44faa73258df4803b07da05e2101a2053c1048f0
Carlos Alexandre Zgierski <carloszgierski@gmail.com>
2020-07-10 23:47:32 -0300

    Added Timer Scene for Countdown Timer.
    Added Countdown Timer for the bomb timer.

---
+++ Assets/Defuser/Scripts/CountdownController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownController : MonoBehaviour
+{
+    [SerializeField] 
+    private TextMeshProUGUI textTimer;
+    [SerializeField] 
+    private float time;
+    
+    private void Update()
+    {
+        time -= Time.deltaTime;
+        UpdateTimer();
+    }
+
+    private void UpdateTimer()
+    {
+        if(textTimer != null)
+        {
+            int minutes = Mathf.FloorToInt(time / 60f);
+            int seconds = Mathf.RoundToInt(time % 60f);
+
+            string formatedSeconds = seconds.ToString();
+
+            if (seconds == 60)
+            {
+                seconds = 0;
+                minutes += 1;
+            }
+
+            textTimer.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+        }
+
+        if(time <= 0)
+        {
+            textTimer.text = "00:00";
+        }
+    }
+}
