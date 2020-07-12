479343afd8febeef56994514cf8bf46551da8710
Carlos Alexandre Zgierski <carloszgierski@gmail.com>
2020-07-12 04:49:04 -0300

    Updated map and F pop-up.

---
+++ Assets/Map/Scripts/BoxOpen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BoxOpen : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _hotkeyText;
+    private bool working;
+
+    void Update()
+    {
+        if(tag != "ItemBox")
+        {
+            _hotkeyText.text = "";
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.tag == "Player" && tag == "ItemBox")
+        {
+            _hotkeyText.text = "F";
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.tag == "Player")
+        {
+            _hotkeyText.text = "";
+        }
+    }
+}
--- Assets/Player/Scripts/PlayerBoxOpener.cs
+++ Assets/Player/Scripts/PlayerBoxOpener.cs
@@ -7,10 +7,7 @@ public class PlayerBoxOpener : MonoBehaviour
 {
     [SerializeField]
     private List <ItemData> toolsPrefabs;
-    [SerializeField]
-    private TextMeshProUGUI _hotkeyText;
     private Collider2D savedCollider;
-    
 
 
     // Update is called once per frame
@@ -19,7 +16,6 @@ public class PlayerBoxOpener : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.F) && savedCollider != null)
         {
             Debug.Log("aperto");
-            // _hotkeyText.text = "";
             toolsPrefabs[Random.Range(0, toolsPrefabs.Count)].Create(transform.position);
             savedCollider.tag = "Untagged";
             savedCollider = null;
@@ -30,7 +26,6 @@ public class PlayerBoxOpener : MonoBehaviour
     {
         if(collider2D.tag == "ItemBox")
         {
-            // _hotkeyText.text = "F";
             savedCollider = collider2D;
             Debug.Log("colidiu");
         }
